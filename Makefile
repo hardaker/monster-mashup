63d0b08f9f69dcef6ce349cf241c3e5c18c269fc
Wes Hardaker <opensource@hardakers.net>
2018-12-12 22:56:33 -0800

    initial version of the "game"

---
+++ Makefile
@@ -0,0 +1,2 @@
+game: game.cpp
+	g++ -o $@ $<
---
+++ game.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <time.h>
+
+using namespace std;
+
+class Monster {
+ private:
+   string name;
+   int hp;
+   bool dead;
+
+ public:
+   Monster(string name, int hp) {
+       this->name = name;
+       this->hp = hp;
+       this->dead = false;
+   }
+
+   string get_name() { return name; };
+
+   virtual int attacks() {
+       // create a random damage amount
+       int damage = rand() % 10;
+
+       // print what happens
+       std::cout << "The " << name << " attacks dealing "
+                 << damage << " damage" << std::endl;
+       return damage;
+   }
+
+   virtual void defends(int damage) {
+       // print what happens
+       std::cout << "You attack the " << name << " dealing " <<
+           damage << " damage" << std::endl;
+
+       // actually apply the damage
+       this->hp -= damage;
+
+       // oh no, did I die?
+       if (this->hp <= 0) {
+           std::cout << "   ARGGGGG...  it's dead!  You win!" << endl;
+           dead = true;
+       }
+   }
+
+   bool is_dead() {
+       return dead;
+   }
+};
+
+class Unicorn : public Monster {
+ public:
+   Unicorn() : Monster("Unicorn", 10) {
+   }
+
+   virtual int attacks() {
+       int damage = rand() % 10;
+       std::cout << "The " << get_name() << " attacks dealing "
+                 << damage << " sparkly rainbow damage points" << std::endl;
+       return damage;
+   }
+
+};
+
+
+main() {
+    srand(time(NULL));
+
+    Monster *m = new Unicorn();
+    while (! m->is_dead()) {
+        m->attacks();
+        m->defends(5);
+    }
+}
