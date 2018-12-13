9d8de27087d4b4ea2800b4dd0de64ce0074d21d1
Wes Hardaker <opensource@hardakers.net>
2018-12-13 12:32:28 -0800

    rewrite with two monsters

--- game.cpp
+++ game.cpp
@@ -9,79 +9,74 @@ class Monster {
  protected:
    string name;
    int hp;
+   int max_damage;
    bool dead;
 
  public:
-   Monster(string name, int hp) {
+   Monster(string name, int max_hp, int max_damage) {
        this->name = name;
-       this->hp = hp;
+       this->hp = (rand()%max_hp);
+       this->max_damage = max_damage;
        this->dead = false;
    }
 
    string get_name() { return name; };
+   int get_hp() { return hp; }
+   bool is_dead() { return dead; }
 
-   virtual int attacks() {
-       // create a random damage amount
-       int damage = rand() % 10;
-
-       // print what happens
-       std::cout << "The " << name << " attacks dealing "
-                 << damage << " damage" << std::endl;
-       return damage;
-   }
-
-   virtual void defends(int damage) {
-       // print what happens
-       std::cout << "You attack the " << name << " dealing " <<
-           damage << " damage" << std::endl;
-
+   virtual bool take_damage(int damage) {
        // actually apply the damage
        this->hp -= damage;
 
        // oh no, did I die?
        if (this->hp <= 0) {
-           std::cout << "   ARGGGGG...  it's dead!  You win!" << endl;
            dead = true;
        }
-   }
-
-   bool is_dead() {
        return dead;
    }
-};
 
-class Unicorn : public Monster {
- public:
-   Unicorn() : Monster("Unicorn", 10) {
+   virtual void print_attack(Monster *defender, int damage) {
+       std::cout << "The " << name << " attacks the "
+                 << defender->get_name() << " dealing "
+                 << damage << " damage" << std::endl;
    }
 
-   virtual int attacks() {
-       int damage = rand() % 10;
-       std::cout << "The " << get_name() << " attacks dealing "
-                 << damage << " sparkly rainbow damage points" << std::endl;
-       return damage;
+   virtual bool attacks(Monster *defender) {
+       // create a random damage amount
+       int damage = rand() % this->max_damage + 1;
+       bool they_died = defender->take_damage(damage);
+
+       // print what happens
+       print_attack(defender, damage);
+       if (they_died) {
+	       std::cout << "   The " << name << " killed the "
+	                 << defender->get_name() << "!!!" << std::endl;
+       } else {
+	       std::cout << "   The " << defender->get_name() << " has "
+	                 << defender->get_hp() << " hit-points left"
+	                 << std::endl;
+	       
+       }
+       return they_died;
    }
 
 };
 
 class Robot : public Monster {
  public:
-   Robot() : Monster("Robot", 10) {
+   Robot() : Monster("Robot", 100, 1) {
+   }
+};
+
+class Unicorn : public Monster {
+ public:
+   Unicorn() : Monster("Unicorn", 30, 5) {
    }
 
-   virtual void defends(int damage) {
-       // print what happens
-       std::cout << "You attack the " << name << "'s metal hulk dealing " <<
-           damage << " damage" << std::endl;
-
-       // actually apply the damage
-       this->hp -= damage;
-
-       // oh no, did I die?
-       if (this->hp <= 0) {
-           std::cout << "   ARGGGGG...  it's disfunctional!  You win!" << endl;
-           dead = true;
-       }
+   virtual void print_attack(Monster *defender, int damage) {
+       std::cout << "The " << name << " attacks the "
+                 << defender->get_name() << " with death-rainbows and deals "
+                 << damage << " pink heart damage" << std::endl;
    }
 
 };
@@ -89,9 +84,18 @@ class Robot : public Monster {
 main() {
     srand(time(NULL));
 
-    Monster *m = new Robot();
-    while (! m->is_dead()) {
-        m->attacks();
-        m->defends(5);
+    Monster *r = new Robot();
+    Monster *u = new Unicorn();
+
+    while(true) {
+        if (r->attacks(u)) {
+	    std::cout << "The " << r->get_name() << " wins the battle!" << std::endl;
+	    break;
+        }
+        if (u->attacks(r)) {
+	    std::cout << "The " << u->get_name() << " wins the battle!" << std::endl;
+	    break;
+        }
+        std::cout << std::endl;
     }
 }
